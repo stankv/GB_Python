4dd96042e15cb58a8e43715aeefb353bd4e8f163
Stan Bad <stankv@mail.ru>
2023-02-19 13:20:33 +0300

    Ф-я, вычисляющая эл-т таблицы по номеру строки и столбца

--- Lesson7/task2.py
+++ Lesson7/task2.py
@@ -4,10 +4,14 @@
 # Аргументы num_rows и num_columns указывают число строк и столбцов таблицы, которые должны быть распечатаны.
 # Нумерация строк и столбцов идет с единицы.
 
+import os
+os.system('cls')
+
+
 def print_operation_table(operation, num_rows=6, num_columns=6):
     for i in range(1, num_rows + 1):
         for j in range(1, num_columns + 1):
-            print(operation(i, j), end=" ")
+            print(operation(i, j), end="\t")
         print()
 
 
